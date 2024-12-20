ebc19bf93156eefbaec9041e5e373d715e4b4b82
CodeReacher <58017164+sree2694@users.noreply.github.com>
2024-12-20 14:38:41 +0530

    initializer started

---
+++ logreg/src/main/java/com/reg/logreg/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.reg.logreg.repository;
+
+import com.reg.logreg.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+}
---
+++ logreg/src/test/java/com/reg/logreg/LogregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reg.logreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
